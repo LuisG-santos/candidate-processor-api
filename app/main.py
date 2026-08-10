14bea36e8d87799d28da2b8523f023eb9e97d86f
LuisG-santos <luisgustavo.nunes@icloud.com>
2026-08-10 01:19:08 -0300

    chore: add initial setup

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="Candidate Processor BTG");
+
+@app.get('/')
+def home():
+    return{"mensagem:": "Hello word"}
+
+@app.post('/jobs')
+def upload():
+    return{"message": "upload"}
+    
