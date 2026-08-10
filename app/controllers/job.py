e4ac8fb06730e949df91cb13e4c9b60fc9b8ffa0
LuisG-santos <luisgustavo.nunes@icloud.com>
2026-08-10 15:38:11 -0300

    feat: update create_job method to return JobCreateResponse

--- app/controllers/job.py
+++ app/controllers/job.py
@@ -1,7 +1,8 @@
+from uuid import UUID
+
 from fastapi import HTTPException, status
 
-from app.models.job import JobModel
-from app.schemas.job import JobCreate
+from app.schemas.job import JobCreate, JobCreateResponse
 from app.services.job import JobService
 
 
@@ -9,11 +10,20 @@ class JobController:
     def __init__(self, job_service: JobService):
         self.service = job_service
 
-    def create_job(self, data: JobCreate) -> JobModel:
+    def create_job(self, data: JobCreate) -> JobCreateResponse:
         try:
             new_job = self.service.create_job(data)
             return new_job
         except ValueError as error:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST, detail=str(error)
+            )
+
+    def get_job(self, job_id: UUID):
+        try:
+            job = self.service.get_job(job_id)
+            return job
+        except ValueError as error:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail=str(error)
             )
