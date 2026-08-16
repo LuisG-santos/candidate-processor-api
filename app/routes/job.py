8a35215e1c807d88edcb082a856ece223e057816
LuisG-santos <luisgustavo.nunes@icloud.com>
2026-08-16 19:38:51 -0300

    feat: Add endpoint to retrieve candidates by job ID

--- app/routes/job.py
+++ app/routes/job.py
@@ -2,8 +2,11 @@ from uuid import UUID
 
 from fastapi import APIRouter, Depends, status
 
+from app.controllers.candidates import CandidateController
 from app.controllers.job import JobController
+from app.dependencies.candidates import get_candidates_controller
 from app.dependencies.job import get_job_controller
+from app.schemas.candidates import CandidateResponse
 from app.schemas.job import JobCreate, JobCreateResponse, JobResponse
 
 router = APIRouter(prefix="/job")
@@ -23,3 +26,15 @@ def get_job(
     controller: JobController = Depends(get_job_controller),  # noqa: B008
     ):  
     return controller.get_job(job_id)
+
+
+@router.get(
+    "/{job_id}/candidates",
+    response_model=list[CandidateResponse],
+    status_code=status.HTTP_200_OK,
+)
+def get_by_job_id(
+    job_id: UUID,
+    controller: CandidateController = Depends(get_candidates_controller),  # noqa: B008
+):
+    return controller.get_by_job_id(job_id)
