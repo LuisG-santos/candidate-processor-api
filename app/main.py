e91211888139898822b440147adec0d8e4ec8df6
LuisG-santos <luisgustavo.nunes@icloud.com>
2026-08-16 21:30:04 -0300

    feat: Add CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes.job import router as job_router
 
 app = FastAPI(title="Candidate Processor BTG")
+
+origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=origins, allow_headers=["*"], 
+    allow_methods=["*"]
+)
 
 app.include_router(job_router)
