05b527464c3388cc795b35d51852a3008d73e6ab
LuisG-santos <luisgustavo.nunes@icloud.com>
2026-08-17 01:04:48 -0300

    feat: Update generate_upload_url to include ContentType parameter

--- app/aws/s3.py
+++ app/aws/s3.py
@@ -4,18 +4,12 @@ from app.config.settings import settings
 
 
 def generate_upload_url(bucket_name: str, s3_key: str) -> str:
-    s3 = boto3.client('s3', region_name=settings.aws_region)
+    s3 = boto3.client("s3", region_name=settings.aws_region)
 
     url = s3.generate_presigned_url(
-        ClientMethod="put_object", 
-        Params={'Bucket': bucket_name, 'Key': s3_key},
-        ExpiresIn=3600
-        )
-    
+        ClientMethod="put_object",
+        Params={"Bucket": bucket_name, "Key": s3_key, "ContentType": "text/csv"},
+        ExpiresIn=3600,
+    )
+
     return url
-    
-
-
-
-
-
